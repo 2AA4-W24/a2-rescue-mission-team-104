46ecb7059345e35b7320d284610005947458d497
Nour-Fawaz <nourfawaz4321@gmail.com>
2024-02-25 18:23:02 -0500

    cleaned up Explorer

--- src/main/java/ca/mcmaster/se2aa4/island/team104/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team104/Explorer.java
@@ -63,92 +63,9 @@ public class Explorer implements IExplorerRaid {
     public String takeDecision() {
         JSONObject decision = new JSONObject();
 
-        ////////   code added    ///////
-
-//        logger.info("this is start: " + start + " this is found: " + found);
-
-        //this tells if the drone should stop/fly/echo --> will keep flying until range = 0
-//        if (Objects.equals(next_action, "stop")) {
-//            decision.put("action", "stop");
-//        }
-//        else if (Objects.equals(next_action, "fly")) {
-//            decision.put("action", "fly");
-//            next_action = "echo";
-//            if (budget < 30) {
-//                next_action = "stop";
-//            }
-//        }
-//        else if (Objects.equals(next_action, "heading")) {
-//            String go_in_direction = null;
-//            init_heading = false;
-//
-//            //checks in which direction it can turn in
-//            for (int i = 0; i < can_go.length; i++) {
-//                if (can_go[i]) {
-//                    if (i == 0) {
-//                        go_in_direction = "N";
-//                    }
-//                    else if (i == 1) {
-//                        go_in_direction = "E";
-//                    }
-//                    else if (i == 2) {
-//                        go_in_direction = "S";
-//                    }
-//                    else if (i == 3) {
-//                        go_in_direction = "W";
-//                    }
-//                }
-//            }
-//            decision.put("action", "heading");
-//            JSONObject parameter = new JSONObject();
-//            parameter.put("direction", go_in_direction);
-//            decision.put("parameters",parameter);
-//            next_action = "fly";
-//
-//        }
-//        else if (Objects.equals(next_action, "echo")) {
-//            JSONObject checker = new JSONObject();
-//
-//            decision.put("action", "echo");
-//
-//            if (begin == 3) {
-//                init_heading = false;
-//            }
-//
-//            if (init_heading) {
-//
-//                switch (initial_head) {
-//                    case ("E"):
-//                        if (begin == 0) {
-//                            checker.put("direction", "E");
-//                            begin++;
-//                        }
-//                        else if (begin == 1) {
-//                            checker.put("direction", "S");
-//                            begin++;
-//                        }
-//                        else if (begin == 2) {
-//                            checker.put("direction", "N");
-//                            begin++;
-//                        }
-//                }
-//            }
-//            else {
-//                checker.put("direction", "E");
-//            }
-//            decision.put("parameters", checker);
-//            next_action = "fly";
-//        }
-
-        ///////////////////////////////////////////////////////////////
         JSONObject next_action = dm.nextAction();
 
 
-
-
-
-//        decision.put("action", "stop"); // we stop the exploration immediately
-//        logger.info("** Decision: {}",decision.toString());
 //        return decision.toString();
         return next_action.toString();
     }
@@ -164,41 +81,6 @@ public class Explorer implements IExplorerRaid {
         JSONObject extraInfo = response.getJSONObject("extras");
         logger.info("Additional information received: {}", extraInfo);
 
-
-        //////     code added      //////
-
-        //this code fetches the value of range
-//            if (!extraInfo.isEmpty()) {
-//                found = extraInfo.getString("found");
-//                range = extraInfo.getInt("range");
-//                logger.info("this is range: " + range);
-//                if (range == 0 && !init_heading) {
-//                    next_action = "stop";
-//                }
-//                else if (begin == 3) {
-//                    next_action = "fly";
-//                }
-//
-//
-//
-//            //only checks for initial heading
-//            if (Objects.equals(found, "GROUND") && !init_heading) {
-//                next_action = "heading";
-//                if (Objects.equals(flight_direction, "N")) {
-//                    can_go[0] = true;
-//                }
-//                else if (Objects.equals(flight_direction, "E")) {
-//                    can_go[1] = true;
-//                }
-//                else if (Objects.equals(flight_direction, "S")) {
-//                    can_go[2] = true;
-//                }
-//                else if (Objects.equals(flight_direction, "W")) {
-//                    can_go[3] = true;
-//                }
-//
-//            }
-//        }
 
 
         ////////////////////////////////////
