0762e8119f5fc0895e0200b397e8c28a7b4bcb54
humnasaeed <humna2004@gmail.com>
2024-02-29 17:03:34 -0500

    Finding Site Skeleton

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team104/FindSite.java
@@ -0,0 +1,44 @@
+package ca.mcmaster.se2aa4.island.team104;
+
+import java.util.ArrayList;
+
+public class FindSite {
+
+    private Actions read_status = Actions.FLY;
+
+
+    private ArrayList<Integer> startingPosition; 
+
+
+    // Island starting position 
+
+
+    public FindSite(ArrayList<Integer> startingPosition, Integer X, Integer Y){
+        this.startingPosition = startingPosition; 
+
+
+
+    }
+
+
+    public void confirmaSite(){
+
+
+
+    }
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
+}
