f8f8d00c76cec4797240dd5229f152c50fc6f1b3
humnasaeed <humna2004@gmail.com>
2024-03-09 20:09:34 -0500

    POI distance skeleton

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team104/FindingDistance.java
@@ -0,0 +1,14 @@
+package ca.mcmaster.se2aa4.island.team104;
+
+// This is a temporary class - might be implemented as a method instead 
+
+public class FindingDistance {
+
+    public void FindingDistance(){
+
+        
+
+
+    }
+
+}
