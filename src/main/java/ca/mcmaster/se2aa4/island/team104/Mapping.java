ed0655d70bf91a310f6dbf0b43611fa6738fa394
Nour-Fawaz <nourfawaz4321@gmail.com>
2024-03-10 11:10:02 -0400

    Revert "Merge pull request #41 from 2AA4-W24/position"
    This reverts commit 4f3f30a50fe7a8e4ebbe78cc78f8e5485193d88a, reversing
    changes made to 8aca7257481a7ee89898f8aabe07d6139a03385e.

--- src/main/java/ca/mcmaster/se2aa4/island/team104/Mapping.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team104/Mapping.java
@@ -1,4 +1,5 @@
 package ca.mcmaster.se2aa4.island.team104;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +8,7 @@ import java.util.Objects;
 public class Mapping {
 
     Map<Position, Tiles> pois = new HashMap<>();
+
 
     Position position = new Position();
     Orientation heading = Orientation.N;
@@ -25,47 +27,36 @@ public class Mapping {
 
     void updateTile(Statistics stats) {
 
-        Tiles tile_type;
+        Tiles tile_type = null;
 
         //for creek
         if (stats.getCreekBool()) {
             tile_type = Tiles.CREEK;
         }
+
         //for site
         else if (stats.getSiteBool()) {
             tile_type = Tiles.SITE;
             stats.site_found = false; //make it false now that it is marked
         }
+
         //for ground
         else if (Objects.equals(stats.getFound(), "GROUND")) {
             tile_type = Tiles.GROUND;
         }
+
         //for home base
         else if (Objects.equals(stats.getState(), State.INIT)) {
             tile_type = Tiles.HOMEBASE;
         }
+
         //for water
         else {
             tile_type = Tiles.WATER;
         }
+
         //put the poi in the map
         pois.put(position, tile_type);
-    }
-
-    void updatePosition(Actions action) {
-
-        if (action == Actions.FLY) {
-            position.updateForward();
-        }
-        else if (action == Actions.HEADING_LEFT) {
-            position.updateForward();
-            position.updateLeft();
-        }
-        else if (action == Actions.HEADING_RIGHT) {
-            position.updateForward();
-            position.updateRight();
-        }
-
     }
 
     void updatePosition(Actions action) {
