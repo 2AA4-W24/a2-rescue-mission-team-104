9981db6825a43e9885e98bbaa629eb82285042c1
Nour-Fawaz <nourfawaz4321@gmail.com>
2024-03-23 15:23:23 -0400

    fixed all test cases for Mapping and fixed bugs inControllerTest (close #80)

--- src/test/java/ca/mcmaster/se2aa4/island/team104/exploration/ControllerTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team104/exploration/ControllerTest.java
@@ -2,13 +2,10 @@ package ca.mcmaster.se2aa4.island.team104.exploration;
 
 import ca.mcmaster.se2aa4.island.team104.drone.Drone;
 import ca.mcmaster.se2aa4.island.team104.drone.Orientation;
-import ca.mcmaster.se2aa4.island.team104.map.Mapping;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,17 +13,13 @@ public class ControllerTest {
 
     private final Logger logger = LogManager.getLogger();
 
-    private Mapping map = new Mapping();
-    private static Drone drone = new Drone();
-    private Controller controller = new Controller(drone);
-
-    @BeforeAll
-    public static void init() {
-        drone.setHeading(Orientation.E);
-    }
-    
     @Test
     public void testActionToJSONHeading() {
+        Drone drone = new Drone();
+        Controller controller = new Controller(drone);
+
+        //set drone heading
+        drone.setHeading(Orientation.E);
 
         JSONObject ret_object = new JSONObject();
         JSONObject direction = new JSONObject();
@@ -40,6 +33,11 @@ public class ControllerTest {
 
     @Test
     public void testEchoRight() {
+        Drone drone = new Drone();
+        Controller controller = new Controller(drone);
+
+        //set drone heading
+        drone.setHeading(Orientation.E);
 
         JSONObject ret_object = new JSONObject();
         JSONObject direction = new JSONObject();
