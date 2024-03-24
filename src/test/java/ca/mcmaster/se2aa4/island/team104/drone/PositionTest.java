61c982503416db872c6cd1cb4060d24a0bf7b1ae
humnasaeed <humna2004@gmail.com>
2024-03-24 17:56:48 -0400

    Fix position class testcases

--- src/test/java/ca/mcmaster/se2aa4/island/team104/drone/PositionTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team104/drone/PositionTest.java
@@ -1,8 +1,8 @@
 package ca.mcmaster.se2aa4.island.team104.drone;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
 
 import ca.mcmaster.se2aa4.island.team104.drone.Position;
 
@@ -11,20 +11,27 @@ import java.util.ArrayList;
 
 public class PositionTest {
 
-    
     @Test
     public void settingDirection() {
 
         Position position = new Position();
-
         String direction = "N"; 
         position.setOrientation(direction);
-        //assertEquals(Orientation.N, Orientation.N.giveOrientation(direction));
-        assertEquals(Orientation.N, position.current_orient); // accessing variable inside class.
-
+        assertEquals(Orientation.N, position.current_orient); 
     }
 
     
+
+    @Test 
+    public void movingRight(){
+
+        Position position = new Position();
+        Integer initial_X = position.getX();
+        position.updateRight(); 
+        assertEquals(initial_X+1,position.getX());
+        assertEquals(Orientation.E, position.current_orient);
+    }
+
     @Test 
     public void movingForward(){
 
@@ -32,21 +39,7 @@ public class PositionTest {
         Integer initial_Y = position.getY(); 
         position.updateForward();
         assertEquals(initial_Y+1, position.getY());
-
     }
-
-    @Test 
-    public void movingRight(){
-
-        Position position = new Position();
-
-        Integer initial_X = position.getX();
-        position.updateRight(); 
-        assertEquals(initial_X+1,position.getX());
-        assertEquals(Orientation.E, position.current_orient);
-
-    }
-
     @Test 
     public void movingLeft(){
 
@@ -56,7 +49,6 @@ public class PositionTest {
         position.updateLeft();
         assertEquals(initial_X-1, position.getX());
         assertEquals(Orientation.W, position.current_orient); 
-
     }
 
     @Test 
@@ -65,9 +57,5 @@ public class PositionTest {
         Position position = new Position();
         Double calc_distance = position.diffBtwnPoints(2,6,2,5);
         assertEquals(5.0, calc_distance); 
-
     }
-
-
-
 }
