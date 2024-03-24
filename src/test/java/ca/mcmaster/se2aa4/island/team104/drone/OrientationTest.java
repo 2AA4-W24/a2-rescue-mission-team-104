53c1df35fc5b6ef6c2d67bd0d42d13b325f19f44
humnasaeed <humna2004@gmail.com>
2024-03-24 18:00:30 -0400

    Refactoring and removing null, close #93

--- src/test/java/ca/mcmaster/se2aa4/island/team104/drone/OrientationTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team104/drone/OrientationTest.java
@@ -15,57 +15,38 @@ public class OrientationTest {
 
         String direction = "E";
         assertEquals(Orientation.E, orient1.giveOrientation(direction)); 
-        assertEquals(null, orient1.giveOrientation("No Direction"));
-
-//        DecisionMaker dm = new DecisionMaker();
-//        String current_head = "E";
-//        dm.stats.heading = Orientation.N;
-//        dm.stats.heading = dm.stats.heading.giveOrientation(current_head);
-//        assertEquals(Orientation.E, dm.stats.heading);
     }
-
 
     @Test
     public void turningRight() {
 
-        // Beginning Orientation East 
+
         Orientation orient1 = Orientation.E;
         assertEquals(Orientation.S, orient1.turnRight());
         assertEquals(Orientation.E, orient1);
 
-        // Beginning Orientation West
+      
         Orientation orient2 = Orientation.W; 
         assertEquals(Orientation.N, orient2.turnRight());
-
-        // Beginning Orientation North
 
         Orientation orient3 = Orientation.N;
         assertEquals(Orientation.E, orient3.turnRight());
 
-        // Beginning Orientation South 
-
         Orientation orient4 = Orientation.S;
         assertEquals(Orientation.W, orient4.turnRight());
-
     }
 
     @Test
     public void turningLeft() {
 
-        // Beginning Orientation East 
         Orientation orient1 = Orientation.E;
         assertEquals(Orientation.N, orient1.turnLeft());
-
-        // Beginning Orientation West
+      
         Orientation orient2 = Orientation.W; 
         assertEquals(Orientation.S, orient2.turnLeft()); 
 
-        // Beginning Orientation North
-
         Orientation orient3 = Orientation.N;
         assertEquals(Orientation.W, orient3.turnLeft());
-
-        // Beginning Orientation South 
 
         Orientation orient4 = Orientation.S;
         assertEquals(Orientation.E, orient4.turnLeft());
@@ -91,5 +72,4 @@ public class OrientationTest {
         assertEquals(Orientation.E, orient1);
 
     }
-
 }
