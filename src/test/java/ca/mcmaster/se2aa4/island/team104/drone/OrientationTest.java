cd232030366fbab0e5e1266a73102e7e516aee81
humnasaeed <humna2004@gmail.com>
2024-03-24 10:22:25 -0400

    Orientation Testing

--- src/test/java/ca/mcmaster/se2aa4/island/team104/drone/OrientationTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team104/drone/OrientationTest.java
@@ -11,6 +11,12 @@ public class OrientationTest {
     @Test
     public void giveDirection() {
 
+        Orientation orient1 = Orientation.E;
+
+        String direction = "E";
+        assertEquals(Orientation.E, orient1.giveOrientation(direction)); 
+        assertEquals(null, orient1.giveOrientation("No Direction"));
+
 //        DecisionMaker dm = new DecisionMaker();
 //        String current_head = "E";
 //        dm.stats.heading = Orientation.N;
@@ -20,19 +26,72 @@ public class OrientationTest {
 
 
     @Test
-    public void turningUsingThis() {
+    public void turningRight() {
 
+        // Beginning Orientation East 
         Orientation orient1 = Orientation.E;
         assertEquals(Orientation.S, orient1.turnRight());
         assertEquals(Orientation.E, orient1);
+
+        // Beginning Orientation West
+        Orientation orient2 = Orientation.W; 
+        assertEquals(Orientation.N, orient2.turnRight());
+
+        // Beginning Orientation North
+
+        Orientation orient3 = Orientation.N;
+        assertEquals(Orientation.E, orient3.turnRight());
+
+        // Beginning Orientation South 
+
+        Orientation orient4 = Orientation.S;
+        assertEquals(Orientation.W, orient4.turnRight());
+
+    }
+
+    @Test
+    public void turningLeft() {
+
+        // Beginning Orientation East 
+        Orientation orient1 = Orientation.E;
+        assertEquals(Orientation.N, orient1.turnLeft());
+
+        // Beginning Orientation West
+        Orientation orient2 = Orientation.W; 
+        assertEquals(Orientation.S, orient2.turnLeft()); 
+
+        // Beginning Orientation North
+
+        Orientation orient3 = Orientation.N;
+        assertEquals(Orientation.W, orient3.turnLeft());
+
+        // Beginning Orientation South 
+
+        Orientation orient4 = Orientation.S;
+        assertEquals(Orientation.E, orient4.turnLeft());
+
+    }
+
+    @Test 
+    public void multipleTurns(){
+
+        Orientation orient1 = Orientation.N;
+
+        Orientation newdir = orient1.turnRight();
+        Orientation newdir2 = newdir.turnRight(); 
+        Orientation newdir3 = newdir2.turnRight(); 
+
+        assertEquals(Orientation.N, newdir3.turnRight()); 
 
     }
 
     @Test
     public void giveOrientStringUsingThis() {
+
         Orientation orient1 = Orientation.E;
         assertEquals("E", orient1.giveStringOrientation());
         assertEquals(Orientation.E, orient1);
+
     }
 
 }
